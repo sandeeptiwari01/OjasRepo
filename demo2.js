ed8455cff6cc7f13d1e285a8517d95d6534ea4d4
sandeeptiwari01 <meet2sandeeptiwari@gmail.com>
2023-03-22 00:21:38 +0530

    demo2code

--- demo2.js
+++ demo2.js
@@ -2,6 +2,15 @@ const http=require('http');
 const data=require('./data')
 
 
+function isInt(num) {
+  return num % 1 === 0;
+}
+
+console.log(isInt(4)); // true
+console.log(isInt(12.2)); // false
+console.log(isInt(0.3)); // false
+
+
 http.createServer((req,resp)=>{
   resp.writeHead(200,{'content-type':'application\json'});
   resp.write(JSON.stringify(data));
